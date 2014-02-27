58f0a5778a24120515b1be4e84ba5cf6524e1281
DanIwao <diwao11@gmail.com>
2014-02-27 10:50:30 -0800

    rspec controller

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -4,21 +4,27 @@ describe JobsController do
 
   describe "GET 'index'" do
     it "returns http success" do
+      Job.stub(:all).and_return('an array of jobs')
       get 'index'
       response.should be_success
+      expect(assigns(:jobs)).to eq 'an array of jobs'
     end
   end
 
   describe "GET 'new'" do
     it "returns http success" do
+      Job.should_receive(:new).and_return("a new job")
       get 'new'
+      expect(assigns(:job)).to eq "a new job"
       response.should be_success
     end
   end
 
   describe "GET 'show'" do
     it "returns http success" do
-      get 'show'
+      Job.should_receive(:find).with("1").and_return("espn")
+      get 'show', {id: 1}
+      expect(assigns(:job)).to eq "espn"
       response.should be_success
     end
   end
