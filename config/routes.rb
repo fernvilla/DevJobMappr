eb820cdf53a35239d26d9dd7b0129a37912396b6
fernvilla <fvilla18@gmail.com>
2014-02-26 16:46:06 -0800

    Add maps controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 DevJobMappr::Application.routes.draw do
+  get "maps/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'maps#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
