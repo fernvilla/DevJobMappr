e414421fdc61f030b1e3841f1e588ce686b2d22c
DanIwao <diwao11@gmail.com>
2014-03-07 16:03:40 -0800

    seed finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,18 +21,18 @@ jobs = Job.create([{job_title: 'Digital Media Designer', company_name: 'Warner B
 
    {job_title: 'Product Manager - Mobile', company_name: 'Hulu', experience_level: 'Senior', job_description: 'Product Manager', primary_languages: 'HTML', job_type: 'Full-Stack', company_address: '2500 Broadway, Santa Monica, CA 90404', job_link: 'http://www.indeed.com/viewjob?jk=0cf8c2faef95e0cb&qd=7qFqBxQMYKBQ0-Rif-TGWFMyD6hKI-GopwBdQPxvop_PLUo1QS8NTLq_J2t7GheABPPUk3V4Z9wiluWgMiCPmoTg27V9mPELotZ6sMq30MfvaYeZxhdRoIjHMJDhtQF8&indpubnum=9335086191163405&atk=18ieq6fve1d723ra'},
 
-   {job_title: 'Software Engineer, Mobile Applications (Android / iOS)', company_name: 'Google', experience_level: 'Senior', job_description: 'Product Manager', primary_languages: 'HTML', job_type: 'Full-Stack', company_address: '604 Arizona Ave, Santa Monica, CA 90401'},
+   {job_title: 'Software Engineer, Mobile Applications (Android / iOS)', company_name: 'Google', experience_level: 'Senior', job_description: 'Product Manager', primary_languages: 'HTML', job_type: 'Full-Stack', company_address: '604 Arizona Ave, Santa Monica, CA 90401', job_link: 'http://www.indeed.com/viewjob?jk=57d987e79edcc894&qd=7qFqBxQMYKBQ0-Rif-TGWFMyD6hKI-GopwBdQPxvop_PLUo1QS8NTLq_J2t7GheABPPUk3V4Z9wiluWgMiCPmoTg27V9mPELotZ6sMq30MfvaYeZxhdRoIjHMJDhtQF8&indpubnum=9335086191163405&atk=18ieq6fve1d723ra'},
 
-   {job_title: 'Programmer', company_name: 'Programmer', experience_level: 'Senior', job_description: 'Developer', primary_languages: 'Python', job_type: 'Full-Stack', company_address: 'Beverly Hills, CA'},
+   {job_title: 'Programmer', company_name: 'LFP Internet Group LLC', experience_level: 'Senior', job_description: 'Developer', primary_languages: 'Python', job_type: 'Full-Stack', company_address: 'Beverly Hills, CA', job_link: 'http://www.indeed.com/viewjob?jk=37a697d93149dd3b&qd=7qFqBxQMYKBQ0-Rif-TGWFMyD6hKI-GopwBdQPxvop_PLUo1QS8NTLq_J2t7GheABPPUk3V4Z9wiluWgMiCPmoTg27V9mPELotZ6sMq30MfvaYeZxhdRoIjHMJDhtQF8&indpubnum=9335086191163405&atk=18ieq6fve1d723ra'},
 
-   {job_title: 'Web Application Developer', company_name: 'Programmer', experience_level: 'Senior', job_description: 'Developer', primary_languages: 'php', job_type: 'Full-Stack', company_address: 'Culver City, CA'},
+   {job_title: 'Web Application Developer', company_name: 'NFL', experience_level: 'Senior', job_description: 'Developer', primary_languages: 'php', job_type: 'Full-Stack', company_address: 'Culver City, CA', job_link: 'http://www.indeed.com/viewjob?jk=0ff4f36db501c74f&qd=7qFqBxQMYKBQ0-Rif-TGWFMyD6hKI-GopwBdQPxvop_PLUo1QS8NTLq_J2t7GheABPPUk3V4Z9wiluWgMiCPmoTg27V9mPELotZ6sMq30MfvaYeZxhdRoIjHMJDhtQF8&indpubnum=9335086191163405&atk=18ieq6fve1d723ra'},
 
-   {job_title: 'Sr. Front End Software Engineer', company_name: 'Programmer', experience_level: 'Senior', job_description: 'Developer', primary_languages: 'Firebug', job_type: 'Full-Stack', company_address: 'Santa Monica, CA'},
+   {job_title: 'Sr. Front End Software Engineer', company_name: 'Programmer', experience_level: 'Senior', job_description: 'Developer', primary_languages: 'Firebug', job_type: 'Full-Stack', company_address: 'Santa Monica, CA', job_link: 'http://www.indeed.com/viewjob?jk=ec1b8fffd16969e8&qd=7qFqBxQMYKBQ0-Rif-TGWFMyD6hKI-GopwBdQPxvop_PLUo1QS8NTLq_J2t7GheABPPUk3V4Z9wiluWgMiCPmoTg27V9mPELotZ6sMq30MfvaYeZxhdRoIjHMJDhtQF8&indpubnum=9335086191163405&atk=18ieq6fve1d723ra'},
 
-   {job_title: 'UI Developer', company_name: 'IT Tech', experience_level: 'Junior', job_description: 'Developer', primary_languages: 'Firebug', job_type: 'Front-End', company_address: '2850 Ocean Park, Santa Monica, CA 90405'},
+   {job_title: 'UI Developer', company_name: 'IT Tech', experience_level: 'Junior', job_description: 'Developer', primary_languages: 'Firebug', job_type: 'Front-End', company_address: '2850 Ocean Park, Santa Monica, CA 90405', job_link: 'http://www.indeed.com/viewjob?jk=353e94e889240ada&qd=7qFqBxQMYKBQ0-Rif-TGWFMyD6hKI-GopwBdQPxvop_PLUo1QS8NTLq_J2t7GheABPPUk3V4Z9wiluWgMiCPmoTg27V9mPELotZ6sMq30MfvaYeZxhdRoIjHMJDhtQF8&indpubnum=9335086191163405&atk=18ieq6fve1d723ra'},
 
 
-   {job_title: 'Web Developer', company_name: 'MV Labs', experience_level: 'Junior', job_description: 'Developer', primary_languages: 'HTML', job_type: 'Back-End', company_address: 'Santa Monica, CA 90405'}
+   {job_title: 'Web Developer', company_name: 'MV Labs', experience_level: 'Junior', job_description: 'Developer', primary_languages: 'HTML', job_type: 'Back-End', company_address: 'Santa Monica, CA 90405', job_link: 'http://www.indeed.com/viewjob?jk=d61b327754f6d72c&qd=7qFqBxQMYKBQ0-Rif-TGWFMyD6hKI-GopwBdQPxvop_PLUo1QS8NTLq_J2t7GheABPPUk3V4Z9wiluWgMiCPmoTg27V9mPELotZ6sMq30MfvaYeZxhdRoIjHMJDhtQF8&indpubnum=9335086191163405&atk=18ieq6fve1d723ra'}
 
   ])
 
