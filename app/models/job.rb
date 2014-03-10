f34bbd531d4abcd5f558526fac4d68e2cf8a2b25
fernvilla <fvilla18@gmail.com>
2014-03-09 20:20:35 -0700

    prep for heroku

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,7 @@
 class Job < ActiveRecord::Base
-	# validates :job_title, :job_description, :experience_level, :job_type, :primary_languages, :job_link, :company_name, :company_address, :latitude, :longitude, presence: {message: "must be present"}
+	validates :job_title, :job_description, :experience_level, :job_type, :primary_languages, :job_link, :company_name, :company_address, :latitude, :longitude, presence: {message: "must be present"}
 
-	# validates :latitude, :longitude, numericality: {message: 'must be a number'}
+	validates :latitude, :longitude, numericality: {message: 'must be a number'}
 
 	geocoded_by :company_address
   after_validation :geocode 
