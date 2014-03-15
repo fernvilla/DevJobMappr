e002ca8c3cb4ffafa3332c437c6200051c796e4c
fernvilla <fvilla18@gmail.com>
2014-03-15 11:42:30 -0700

    Google api key fix

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,10 +1,10 @@
 class Job < ActiveRecord::Base
 	validates :job_title, :job_description, :experience_level, :job_type, :primary_languages, :job_link, :company_name, :company_address, :latitude, :longitude, presence: {message: "must be present"}
 
-	validates :latitude, :longitude, numericality: {message: 'must be a number'}
+	# validates :latitude, :longitude, numericality: {message: 'must be a number'}
 
 	geocoded_by :company_address
-  after_validation :geocode 
+  before_validation :geocode 
 
   belongs_to :user
 
